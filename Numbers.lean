2eaf894d82cacd837fc0b604e3ff71e2ecf6f80b
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-07-09 11:35:05 +0800

    update this

--- Numbers.lean
+++ Numbers.lean
@@ -1,3 +1,11 @@
+import Numbers.Nopush.sol
+import Numbers.Nopush.tactic
+import Numbers.Nopush.talk
+import Numbers.Solutions.integers_no_sorry
+import Numbers.Solutions.rationals_no_sorry
+import Numbers.Solutions.rationals_order_no_sorry
+import Numbers.Solutions.reals_no_sorry
+import Numbers.TypeTheory.Exercises
 import Numbers.TypeTheory.typetheory
 import Numbers.integers
 import Numbers.rationals
